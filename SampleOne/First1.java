5fc6b5e6b20dd090fad3609a8838c8bd12016524
NissyRayapu21 <nissyrayapu@gmail.com>
2025-08-11 13:54:38 +0530

    First commit

---
+++ SampleOne/First1.java
@@ -0,0 +1,22 @@
+package SampleOne;
+
+public class First1 {
+
+	public static void main(String[] args) {
+		
+		int number = 0;
+		 if ( number>0)
+			 System.out.println("num is positive");
+		 else if (number<0)
+				 System.out.println("num is negative");
+		 else
+			 System.out.println("num is zero");
+			 
+			 
+			 }
+		 }
+	
+
+		
+		 
+	
